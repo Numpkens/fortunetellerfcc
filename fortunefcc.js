cc5e2aa2fea6e0dd117ac181f8ab7a2d3b755f33
Numpkens <numpkins188@gmail.com>
2025-05-26 19:26:17 +0800

    "Finish the project"

--- fortunefcc.js
+++ fortunefcc.js
@@ -29,3 +29,24 @@ let fortune3 = "Be cautious of your new neighbours.";
 let fortune4 = "You will find a new hobby soon.";
 let fortune5 = "It would be wise to avoid the color red today.";
 
+const min = 1;
+const max = 5;
+
+// Generate random number between 1 and 5 (inclusive) using Math.round
+const randomNumber = Math.round(Math.random() * (max - min) + min);
+
+let selectedFortune;
+
+if (randomNumber === 1) {
+    selectedFortune = fortune1;
+} else if (randomNumber === 2) {
+    selectedFortune = fortune2;
+} else if (randomNumber === 3) {
+    selectedFortune = fortune3;
+} else if (randomNumber === 4) {
+    selectedFortune = fortune4;
+} else if (randomNumber === 5) {
+    selectedFortune = fortune5;
+}
+
+console.log(selectedFortune);
